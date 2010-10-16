6f80e509a146fe548436f9985bee0bdb70144d57
Zachary Scott <zachary.s.scott@gmail.com>
2010-10-15 22:03:05 -0400

    adding validator type switch

--- kensho.rb
+++ kensho.rb
@@ -8,14 +8,29 @@ require "haml"
 require "w3c_validators"
 
 class Kensho < Sinatra::Base
-  
-  set :public, File.dirname(__FILE__) + '/public'
 
+  include W3CValidators
+
+  set :public, File.dirname(__FILE__) + '/public'
+  
   get '/' do
     haml :index
   end
 
   post '/validate' do
+    case params[:type]
+      when "HTML"
+        @validator = MarkupValidator.new
+        @validator.set_debug!(true)
+      when "FEED"
+        @validator = FeedValidator.new
+      when "CSS"
+        @validator = CSSValidator.new
+        @validator.set_warn_level!(2) 
+      else
+        @validator = nil
+    end
+    @errors = @validator.validate_text(params[:markup]) 
     haml :validate
   end
 
